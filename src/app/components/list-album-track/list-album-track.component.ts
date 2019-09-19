d48e736fe8b31839967769f64696f8652a21277b
Axel Warneke <axelwarneke@gmail.com>
2019-09-19 00:01:05 -0300

    OrderTables

--- src/app/components/list-album-track/list-album-track.component.ts
+++ src/app/components/list-album-track/list-album-track.component.ts
@@ -18,17 +18,33 @@ export class ListAlbumTrackComponent {
 
   }
 
-    checkFav( favSong: string ) {
-      return this.spotify.estadoFav( favSong );
-      }
+  sort() {
+    if (this.items[0].duration_ms > this.items[1].duration_ms) {
+      this.items.sort((a, b)  => a.duration_ms - b.duration_ms);
+    } else {
+      this.items.sort((a, b)  => b.duration_ms - a.duration_ms);
+    }
+  }
+  sortn() {
+    if (this.items[0].track_number > this.items[1].track_number) {
+      this.items.sort((a, b)  => a.track_number - b.track_number);
+    } else {
+      this.items.sort((a, b)  => b.track_number - a.track_number);
+    }
+  }
 
-      checkIfFavourite( favSong: string ) {
-        if (this.spotify.estadoFav( favSong ) === true) {
-          this.spotify.removeFavourite( favSong );
-        } else {
-          this.spotify.favouriteSongs( favSong );
+checkFav( favSong: string ) {
+  return this.spotify.estadoFav( favSong );
+  }
 
-        }
-        }
+  checkIfFavourite( favSong: string ) {
+    if (this.spotify.estadoFav( favSong ) === true) {
+      this.spotify.removeFavourite( favSong );
+    } else {
+      this.spotify.favouriteSongs( favSong );
+
+    }
+    }
+
 
 }
