19c7670b9691ae76539c109ca3a3db4beeb98ef3
Axel <axelwarneke@gmail.com>
2024-02-08 15:10:46 -0300

    prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
+  spotifyClientId: '476b04f286264f229aed7cd9acc85f7e',
+  redirectUri: 'http://spotiwar.s3-website-sa-east-1.amazonaws.com/home',
 };
