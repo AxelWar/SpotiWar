1d82e5bade56dc759f518caf9b8281f7db12df8f
Axel <axelwarneke@gmail.com>
2024-03-01 11:39:57 -0300

    remove unnecesary imports in index.html, replace window.location.ref for router, fix environment replace

--- src/app/shared/components/auth-modal/auth-modal.component.spec.ts
+++ src/app/shared/components/auth-modal/auth-modal.component.spec.ts
@@ -1,7 +1,9 @@
+import { RouterTestingModule } from '@angular/router/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthModalComponent } from './auth-modal.component';
 import { SharedModule } from '../../shared.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AuthModalComponent', () => {
   let component: AuthModalComponent;
@@ -10,7 +12,7 @@ describe('AuthModalComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [AuthModalComponent],
-      imports: [SharedModule],
+      imports: [SharedModule, HttpClientTestingModule, RouterTestingModule],
     });
     fixture = TestBed.createComponent(AuthModalComponent);
     component = fixture.componentInstance;
